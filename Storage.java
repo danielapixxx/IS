d372353e8b74975b1b23c288384249bf351f99b5
Danilo Apicella <d.apicella21@studenti.it>
2017-09-25 12:48:08 +0200

    new commit

--- Storage.java
+++ Storage.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Storage {
-
+	//committed
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 		try{
